7c540b012386cad57b95c2335a726bd58d910e1d
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-04-09 17:50:15 -0300

    chore: create screens

---
+++ StackNavigator.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Chat, Home, Login } from './src/screens';
+
+const Stack = createNativeStackNavigator();
+
+export function StackNavigator() {
+	const user = false;
+	return (
+		<Stack.Navigator>
+			{user ? (
+				<>
+					<Stack.Screen name='Home' component={Home} />
+					<Stack.Screen name='Chat' component={Chat} />
+				</>
+			) : (
+				<Stack.Screen name='Login' component={Login} />
+			)}
+		</Stack.Navigator>
+	);
+}
---
+++ src/screens/Chat/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function Chat() {
+	return (
+		<View>
+			<Text>Chat screen</Text>
+		</View>
+	);
+}
