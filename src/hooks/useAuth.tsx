63f125c78906782e667db931e2b760bdf4c75984
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-04-15 10:03:27 -0300

    chore: pass sign in function by context

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,18 +1,33 @@
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, ReactNode, useContext, useEffect } from 'react';
 
 interface UserData {
 	user?: string;
+	signInWithGoogleAsync: () => Promise<void>;
 }
-const AuthContext = createContext<UserData>({});
+
+const AuthContext = createContext<UserData>({} as UserData);
+
+const config = {
+	iosClientId:
+		'854451341634-hqiqc585gp2ere73d40v40q3gtgua5kl.apps.googleusercontent.com',
+	androidClientId:
+		'854451341634-c0u408k8ctu2ue806n381a0hmh1g39r3.apps.googleusercontent.com',
+	scopes: ['profile', 'email'],
+	permissions: ['public_profile', 'email', 'gender', 'location'],
+};
 
 interface AuthProviderProps {
 	children: ReactNode;
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
+	async function signInWithGoogleAsync() {
+		
+	};
+
 	return (
 		<>
-			<AuthContext.Provider value={{ user: 'Luara' }}>
+			<AuthContext.Provider value={{ user: undefined, signInWithGoogleAsync }}>
 				{children}
 			</AuthContext.Provider>
 		</>
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,13 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import useAuth from '../../hooks/useAuth';
 
 export function Login() {
-	const { user } = useAuth();
+	const { user, signInWithGoogleAsync } = useAuth();
 
 	return (
 		<View>
 			<Text>Login screen {user}</Text>
+			<Button title='Sign in with Google' onPress={signInWithGoogleAsync} />
 		</View>
 	);
 }
