f48bf683b2099606c9c8848d19cad8314073bf96
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-04-15 14:21:28 -0300

    chore: add authentication with google

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/hooks/useAuth';
 import { StackNavigator } from './src/routes/StackNavigator';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
 	return (
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,14 +1,26 @@
-import { createContext, ReactNode, useContext, useEffect } from 'react';
-interface UserData {
-	user?: string;
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import * as AuthSession from 'expo-auth-session';
+import { Alert } from 'react-native';
+
+interface User {
+	id: string;
+	name: string;
+	firstName?: string;
+	email: string;
+	pictureUrl?: string;
+}
+interface ContextData {
+	user?: User;
 	signInWithGoogleAsync: () => Promise<void>;
 }
 
-const AuthContext = createContext<UserData>({} as UserData);
+const AuthContext = createContext<ContextData>({} as ContextData);
 
-const config = {
-	iosClientId: '',
-	androidClientId: '',
+type AuthResponse = {
+	type: string;
+	params: {
+		access_token: string;
+	};
 };
 
 interface AuthProviderProps {
@@ -16,11 +28,48 @@ interface AuthProviderProps {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-	async function signInWithGoogleAsync() {}
+	const [user, setUser] = useState<User>();
+
+	async function loadProfile(accessToken: string) {
+		const response = await fetch(
+			`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${accessToken}`
+		);
+		const data = await response.json();
+
+		setUser({
+			id: data.id,
+			name: data.name,
+			firstName: data.given_name,
+			email: data.email,
+			pictureUrl: data.picture,
+		});
+	}
+
+	async function signInWithGoogleAsync() {
+		const CLIENT_ID =
+			'648392820231-a6n4vk7ll1akkpurh88qog2n56camtq2.apps.googleusercontent.com';
+		const REDIRECT_URI = 'https://auth.expo.io/@luarakerlen/tinder-clone';
+		const RESPONSE_TYPE = 'token';
+		const SCOPE = encodeURI('profile email');
+
+		const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+		const { type, params } = (await AuthSession.startAsync({
+			authUrl,
+		})) as AuthResponse;
+
+		if (type === 'success') {
+			await loadProfile(params.access_token);
+		} else {
+			Alert.alert(
+				'Erro ao logar',
+				'Não foi possível fazer login com a conta Google'
+			);
+		}
+	}
 
 	return (
 		<>
-			<AuthContext.Provider value={{ user: undefined, signInWithGoogleAsync }}>
+			<AuthContext.Provider value={{ user, signInWithGoogleAsync }}>
 				{children}
 			</AuthContext.Provider>
 		</>
