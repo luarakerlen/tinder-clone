dbe28ec2cf36ce8efb7a43177bd33702edba44f9
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-04-15 10:40:20 -0300

    chore: delete firebase

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,5 +1,4 @@
 import { createContext, ReactNode, useContext, useEffect } from 'react';
-
 interface UserData {
 	user?: string;
 	signInWithGoogleAsync: () => Promise<void>;
@@ -8,12 +7,8 @@ interface UserData {
 const AuthContext = createContext<UserData>({} as UserData);
 
 const config = {
-	iosClientId:
-		'854451341634-hqiqc585gp2ere73d40v40q3gtgua5kl.apps.googleusercontent.com',
-	androidClientId:
-		'854451341634-c0u408k8ctu2ue806n381a0hmh1g39r3.apps.googleusercontent.com',
-	scopes: ['profile', 'email'],
-	permissions: ['public_profile', 'email', 'gender', 'location'],
+	iosClientId: '',
+	androidClientId: '',
 };
 
 interface AuthProviderProps {
@@ -21,9 +16,7 @@ interface AuthProviderProps {
 }
 
 export function AuthProvider({ children }: AuthProviderProps) {
-	async function signInWithGoogleAsync() {
-		
-	};
+	async function signInWithGoogleAsync() {}
 
 	return (
 		<>
